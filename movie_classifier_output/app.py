52d5f2b6c4cfcf48badf4a6c127f13ec56fe5603
Manimaran-SM <51485469+Manimaran-SM@users.noreply.github.com>
2020-03-11 22:06:53 +0530

    Second commit

--- movie_classifier_output/app.py
+++ movie_classifier_output/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 from Output import predict
 
 app = Flask(__name__)
-inputs=[]
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -14,7 +13,6 @@ def submit():
         return render_template('index.html',message='please enter text Review')
     else:
         Prediction,percentage=predict(search)
-        input=append(search)
         return render_template('index.html',message='Prediction: %s <br> Probability: %.2f%%' %(Prediction,percentage))
 
 if __name__ == '__main__':
